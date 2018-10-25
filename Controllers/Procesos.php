c68f605f32ea1756e6daa594097bb102b68c6355
jarryp <jarry.palacios@hotmail.com>
2018-10-25 07:43:05 -0400

    Creacion de controlador de procesos

---
+++ Controllers/Procesos.php
@@ -0,0 +1,26 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+class Procesos extends Controllers {
+
+	function __construct(){
+		parent::__construct();
+	}
+
+	public function procesos(){
+		//echo "Hola desde el Controlador de usarios";
+		$UserName = Session::getSession("usuario");
+
+		if($UserName!=""){
+			$this->view->render($this,'procesos');
+		}else{
+			header("Location:".URL);
+		}
+	}
+
+	public function saluda(){
+		echo "Hola desde controlador de procesos";
+	}
+
+
+}
